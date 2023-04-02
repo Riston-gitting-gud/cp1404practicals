38bb7aff81667391aa2bf1cad23cc02168e142ba
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-04-02 12:39:30 +0800

    Random functions test

---
+++ prac_03/randoms.py
@@ -0,0 +1,30 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1?
+# 10
+
+# What was the smallest number you could have seen, what was the largest?
+# Smallest number was 5, largest number was 20.
+
+
+# What did you see on line 2?
+# 5
+
+# What was the smallest number you could have seen, what was the largest?
+# Smallest number was 3, largest number was 9.
+
+# Could line 2 have produced a 4?
+# No.
+
+
+# What did you see on line 3?
+# 3.4478135207636305
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number I could have seen would be 2.5, while the largest could be 5.5.
+
+print(random.randint(1, 100))
