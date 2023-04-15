d5720e0871a7739b275427e4668fcf3ad25ef7f5
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-04-15 17:59:54 +0800

    Store users' emails and names in a dictonary.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,3 +1,37 @@
-# User enter email, if user say yes, get next email. If user say no, get user to enter name.
-# Loop ends with name and email are entered.
-fhyjyfhf
+# Emails
+
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        verification = input(f"Is your name {name}? (Y/n) ").upper()
+        if verification != "Y" and verification != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} {email}")
+
+
+def get_name_from_email(email):
+    prefix = email.split("@")[0]
+    parts = prefix.split(".")
+    name = ""
+    for part in parts:
+        name += part.capitalize()
+    return name
+
+
+main()
+
+
+
+
+
+
+
+
+
+
