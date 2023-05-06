e29138bd2bd1030aec70856b57ffcb6bf04f565f
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-05-06 17:39:14 +0800

    Complete 2 menu options 'd' to display projects ad 'u' to update projects. Their respective functions, display_all_projects(projects) and update_project(projects) have been created and tested for functionality. (milestone)

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,8 +1,6 @@
 # Project Management Program
 
 import datetime
-
-
 from project import Project
 
 
@@ -23,6 +21,10 @@ def main():
         if user_input.lower() == 'l':
             file_name = input('Please type in the file to be loaded: ')
             projects = load_projects(file_name)
+        if user_input.lower() == 'd':
+            display_all_projects(projects)
+        if user_input.lower() == 'u':
+            update_project(projects)
 
 
 def sort_by_priority(projects, to_reverse):
@@ -45,3 +47,34 @@ def load_projects(file_name):
         print(projects)
         return projects
 
+
+def display_all_projects(projects):
+    incomplete_projects = 'Incomplete projects: \n'
+    complete_projects = 'Completed projects: \n'
+    sorted_projects = sort_by_priority(projects, False)
+
+    for project in sorted_projects:
+        if project.completion_estimate < 100:
+            incomplete_projects = incomplete_projects + '\t' + repr(project) + '\n'
+        else:
+            complete_projects = complete_projects + '\t' + repr(project) + '\n'
+    display = incomplete_projects + complete_projects
+    display = display.strip()  # do not display the last newline
+    print(display)
+
+
+def update_project(projects):
+    projects_str = ''
+    for i, project in enumerate(projects):
+        projects_str = projects_str + str(i) + ' ' + repr(project) + '\n'
+    # # do not display the last newline
+    # print(display[:len(display) - 2])
+    print(projects_str)
+    project_choice = int(input('Project choice: '))
+    print(repr(projects[project_choice]))
+    new_percentage = input('New Percentage: ')
+    new_priority = input('New Priority: ')
+    if new_percentage != '':
+        projects[project_choice].completion_estimate = int(new_percentage)
+    if new_priority != '':
+        projects[project_choice].priority = int(new_priority)
