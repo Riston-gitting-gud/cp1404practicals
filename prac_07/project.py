7bcdeeb4e339de0c84cdf4a5bf7b9f10a251cd9e
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-05-06 14:48:25 +0800

    Reconstructed class Project and its essential functions

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,58 +1,33 @@
 # Project Management Program
 
-import datetime
-
-
 class Project:
-    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_estimate, id):
+        """Construct a Project from the given values."""
         self.name = name
-        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.start_date = start_date
         self.priority = priority
         self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
-
-    def __str__(self):
-        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
-
-    def __repr__(self):
-        return str(self)
+        self.completion_estimate = completion_estimate
+        self.id = id
 
     def __lt__(self, other):
         return self.priority < other.priority
 
-    def __eq__(self, other):
-        return self.priority == other.priority
-
     def __gt__(self, other):
         return self.priority > other.priority
 
-    class ProjectList:
-        def __init__(self):
-            self.projects = []
+    def __le__(self, other):
+        return self.priority <= other.priority
 
-        def load_projects(self, filename):
-            with open(filename, 'r') as file:
-                lines = file.readlines()[1:]
-                for line in lines:
-                    name, start_date, priority, cost_estimate, completion_percentage = line.strip().split('\t')
-                    project = Project(name, start_date, int(priority), float(cost_estimate), int(completion_percentage))
-                    self.projects.append(project)
+    def __ge__(self, other):
+        return self.priority >= other.priority
 
-        def save_projects(self, filename):
-            with open(filename, 'w') as file:
-                file.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
-                for project in self.projects:
-                    file.write(f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}"
-                               f"\t{project.priority}\t{project.cost_estimate:.2f}\t{project.completion_percentage}\n")
+    def __eq__(self, other):
+        return self.priority == other.priority
 
-        def display_projects(self):
-            incomplete = sorted([p for p in self.projects if p.completion_percentage < 100])
-            complete = sorted([p for p in self.projects if p.completion_percentage == 100])
-            print("Incomplete projects:")
-            for project in incomplete:
-                print(f"  {project}")
-            print("Completed projects:")
-            for project in complete:
-                print(f"  {project}")
+    def __repr__(self):
+        """Return string representation of a Project."""
+        return f"{self.name}, start: {self.start_date}, priority: {self.priority}," \
+               f" cost_estimate: ${self.cost_estimate:.2f}, completion: {self.completion_estimate}%"
 
 
