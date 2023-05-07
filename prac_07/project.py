87ed10d9e62c0f730ef9820aefd4a669eb6a5b65
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-05-07 12:34:29 +0800

    Added docstrings to describe Project class and Project object comparison functions, as well as time taken to complete the entire program.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,10 @@
-# Project Management Program
+"""
+Project Management Program
+Actual time taken: 12 hours
+"""
 
 class Project:
+    """Project class that stores details about a project"""
     def __init__(self, name, start_date, priority, cost_estimate, completion_estimate, id):
         """Construct a Project from the given values."""
         self.name = name
@@ -11,18 +15,23 @@ class Project:
         self.id = id
 
     def __lt__(self, other):
+        """Show if one project is lesser than another in priority"""
         return self.priority < other.priority
 
     def __gt__(self, other):
+        """Show if one project is greater than another in priority"""
         return self.priority > other.priority
 
     def __le__(self, other):
+        """Determine if a project has a lesser than or equal priority to another project"""
         return self.priority <= other.priority
 
     def __ge__(self, other):
+        """Determine if a project has a greater than or equal priority to another project"""
         return self.priority >= other.priority
 
     def __eq__(self, other):
+        """Determine if a project is equal in priority with another project"""
         return self.priority == other.priority
 
     def __repr__(self):
