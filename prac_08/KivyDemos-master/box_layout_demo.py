7680e68c8dc6b41ab97aa286aa44e08c67a1d35b
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-05-14 19:04:19 +0800

    Added docstrings to describe each function

--- prac_08/KivyDemos-master/box_layout_demo.py
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -6,15 +6,18 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build the BoxLayout  App"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle greeting button press, updates text of name input to username and greets user"""
         print('greet')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def press_clear(self):
+        """Clear name input text"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
