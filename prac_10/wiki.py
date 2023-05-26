627e2a9e10adeca710ec5ac292f192345e60c113
Riston-gitting-gud <jitlongriston.lau@my.jcu.edu.au>
2023-05-26 17:10:24 +0800

    Completed Wikipedia API & Python Library exercise by modifying Wikipedia API program.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,2 +1,22 @@
-print("HI")
-print("TEST")
+
+# Wikipedia API & Python Library
+
+import wikipedia
+
+
+def main():
+    """Wikipedia API program that prints title, summary and URL of a page in Wikipedia"""
+    search_term = None
+    while search_term != "":
+        search_term = input("please input a page title or search phrase:")
+        if search_term != "":
+            try:
+                page_results = wikipedia.page(search_term, auto_suggest=False)
+                print(page_results.title)
+                print(page_results.summary)
+                print(page_results.url)
+            except wikipedia.exceptions.DisambiguationError as e:
+                print(e.options)
+
+
+main()
